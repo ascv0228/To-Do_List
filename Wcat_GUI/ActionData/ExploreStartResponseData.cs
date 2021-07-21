c1e36feadb7076bc8b21d641216cbfae2d1f09f1
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2021-07-21 23:02:31 +0800

    Create ExploreStartResponseData.cs

---
+++ Wcat_GUI/ActionData/ExploreStartResponseData.cs
@@ -0,0 +1,10 @@
+namespace ExploreStartResponseData
+{
+    public class RootObject
+    {
+        public int error { get; set; }
+        public List<object> dummy { get; set; }
+        public int tutorial { get; set; }
+        public DateTime serverDate { get; set; }
+    }
+}

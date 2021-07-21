8ea5598ace8e7146de71ed1ceb9900a5d273f3ad
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2021-07-21 20:11:11 +0800

    Create ExploreUpdateDeckRequestData.cs

---
+++ Wcat_GUI/ActionData/ExploreUpdateDeckRequestData.cs
@@ -0,0 +1,12 @@
+namespace ExploreUpdateDeckRequestData
+{
+    public class RootObject
+    {
+        public int dn { get; set; }
+        public string ueid0 { get; set; }
+        public string ueid1 { get; set; }
+        public string ueid2 { get; set; }
+        public string ueid3 { get; set; }
+    }
+}
+

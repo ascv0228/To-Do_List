e0285827f3dbb867e0c2b24735b45d7fec4260fc
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2021-07-21 20:31:24 +0800

    Create ExploreStartRequestData.cs

---
+++ Wcat_GUI/ActionData/ExploreStartRequestData.cs
@@ -0,0 +1,12 @@
+namespace ExploreStartRequestData
+{
+    public class RootObject
+    {
+        public int dn { get; set; }             //隊伍
+        public int exId { get; set; }           //關卡Id = ExploreQuestListResponseData.RootObject.result.listDatas[0].explores[n].id
+        public string ueid0 { get; set; }
+        public string ueid1 { get; set; }
+        public string ueid2 { get; set; }
+        public string ueid3 { get; set; }
+    }
+}
